de5afd46eb7fa15375618c6b755bf659a13d10b9
Honchar007 <andrey2002335@gmail.com>
2023-08-15 17:04:10 +0300

    Style todo item

--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -2,13 +2,16 @@
   <div class="input-wrapper">
     <CheckboxCustom />
     <input
+      class="input-content"
       :value="modelValue"
       :disabled="disabled"
       @focus="$emit('focus', $event)"
       @input="$emit('update:modelValue', $event.target.value)"
     />
-    <button>Delete</button>
-    <button>Edit</button>
+    <ButtonCustom>+</ButtonCustom>
+    <ButtonCustom>
+      <img src="../assets/close.svg" alt="add new todo" />
+    </ButtonCustom>
   </div>
 </template>
 <script>
@@ -18,11 +21,13 @@ import {
 
 // components
 import CheckboxCustom from './CheckboxCustom.vue';
+import ButtonCustom from './ButtonCustom.vue';
 
 export default defineComponent({
   name: 'ToDoItem',
   components: {
     CheckboxCustom,
+    ButtonCustom,
   },
   props: {
     label: {
@@ -44,5 +49,31 @@ export default defineComponent({
     display: flex;
     flex-direction: row;
     justify-content: center;
+    gap:0.5rem;
+
+    .input-content {
+      position: relative;
+      display: inline-flex;
+      width: 100%;
+      border-radius: 10rem;
+      padding: 0 0.875rem;
+      transition: 0.3s ease-in-out all;
+      font-style: normal;
+      font-weight: 600;
+      font-size: 1rem;
+      line-height: 1.875rem;
+
+      &:focus {
+        box-shadow: 0px 4px 16px rgba(1, 1, 1, 0.2);
+      }
+
+      &:hover {
+        box-shadow: 0px 4px 16px rgba(1, 1, 1, 0.2);
+      }
+
+      &::placeholder {
+        color: rgb(0, 0, 0);
+      }
+    }
   }
 </style>
